d37e0552a42890405fb7800d468e4f69f2e574b6
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2025-05-20 14:58:01 -0300

    feat: create Header component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "@/components/Header";
 
 const Page = () => {
   return (
     <main className="wrapper page">
+      <Header title="Todos os videos" subHeader="Biblioteca" />
       <h1 className="text-2xl font-karla">Bem vindo Gustavo</h1>
     </main>
   );
